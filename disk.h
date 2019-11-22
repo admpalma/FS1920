866db471b84c8e654a8a4ab0db855ba786fc7869
André Palma <andremarquespalma@gmail.com>
2019-11-22 19:08:06 +0000

    Add disk.h Comments

--- disk.h
+++ disk.h
@@ -3,10 +3,20 @@
 
 #define DISK_BLOCK_SIZE 4096
 
+/*This function must be invoked before calling other API functions.	
+It is only possible to have one active disk at some point in time.*/
 int  disk_init( const char *filename, int nblocks );
+
+/*Returns an integer with the total number of the blocks in the disk.*/
 int  disk_size();
+
+/*Reads the contents of the block disk numbered blocknum (4096 bytes) to a memory buffer that starts at address data.*/
 void disk_read( int blocknum, char *data );
+
+/*Writes, in the block blocknum of the disk, a total of 4096 bytes starting at memory address data.*/
 void disk_write( int blocknum, const char *data );
+
+/*Function to be called at the end of the program.*/
 void disk_close();
 
 
